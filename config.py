49b0c17802c56fc82084118a7501be2d281e7905
sdvuk7777 <up.ukpvt111@gmail.com>
2025-03-01 14:05:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-# Telegram Bot Token 
+# Telegram Bot Token
 BOT_TOKEN = "8071764665:AAF5zG5aZtfA0fWAPbDm14LDGbbLxs-dcN0"
 
-# Log group ID 
-LOG_GROUP_ID = -1002385500773
+# Log group IDs for each handler
+LOG_GROUP_ID_PW = -1002369362009  # Replace with your PW log group ID
+LOG_GROUP_ID_AK = -1002452563878  # Replace with your AK log group ID
+LOG_GROUP_ID_KGS = -1002323734449  # Replace with your KGS log group ID
+CLONE_LOG_GROUP_ID = -1002266183672
